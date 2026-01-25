3498140cca3191b172596d46b551e18bdc0967c9
Robin Ryan <robin1ryan@hotmail.com>
2026-01-25 11:12:37 +0200

    updated framermotion layout a bit - increased size

--- src/components/graph/graph.tsx
+++ src/components/graph/graph.tsx
@@ -11,282 +11,6 @@ type Price = {
   y: number;
 };
 
-const GraphOne = () => {
-  const chartContainer = useRef<HTMLDivElement | null>(null);
-
-  const { data, isLoading } = useQuery({
-    queryKey: ["graphData"],
-    queryFn: () => fetchData("/data.json"),
-  });
-
-  useEffect(() => {
-    if (isLoading) return;
-
-    // Clean chart
-    d3.select(chartContainer.current).selectAll("svg").remove();
-
-    const margin = { top: 70, right: 30, bottom: 40, left: 80 };
-    const width = 1200 - margin.left - margin.right;
-    const height = 500 - margin.top - margin.bottom;
-
-    // Main-svg
-    const svg = d3
-      .select(chartContainer.current)
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", `translate(${margin.left},${margin.top})`);
-
-    // tooltip - mouse over svg
-    const tooltip = d3
-      .select("body")
-      .append("div")
-      .append("tooltip")
-      .attr("class", `${classes.tooltip}`);
-
-    // Scales
-    // data.price.x === date
-    const x = d3
-      .scaleTime()
-      .range([0, width])
-      .domain(d3.extent(data.price, (d) => new Date(d.x)) as [Date, Date]);
-
-    // data.price.y === price
-    const yMin = d3.min(data.price, (d) => d.y) as number;
-    const yMax = d3.max(data.price, (d) => d.y) as number;
-    const y = d3
-      .scaleLinear()
-      .domain([yMin - 10000, yMax + 10000])
-      .range([height, 0])
-      .nice();
-
-    svg
-      .append("g")
-      .attr("transform", `translate(0,${height})`)
-      .style("font-size", "12px")
-      .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%H:%M")))
-      .call((g) => g.select(".domain").remove().selectAll(".tick text"))
-      .style("stroke-opacity", 0);
-    svg.selectAll(".tick text").attr("fill", "#777");
-
-    // y-axis
-    svg
-      .append("g")
-      .style("font-size", "12px")
-      .call(
-        d3
-          .axisLeft(y)
-          .ticks(d3.max(data.price, (d) => d.y - yMin) / 4500) // Increase/decrease to adjust visible price-points
-          .tickFormat((d) => {
-            const num: number = d;
-
-            if (num >= 10000) {
-              return `${(num / 1000).toFixed(0)}k`;
-            } else {
-              num.toString();
-            }
-          })
-          .tickSize(0)
-          .tickPadding(10),
-      )
-      .call((g) => g.select(".domain").remove())
-      .selectAll(".tick text")
-      .style("fill", "#777")
-      .style("visibility", (_, i, __) => {
-        if (i === 0) {
-          return "hidden";
-        } else {
-          return "visible";
-        }
-      });
-
-    // x-axis grid-line
-    svg
-      .selectAll("xGrid")
-      .data(x.ticks())
-      .join("line")
-      .attr("x1", (d) => x(d))
-      .attr("x2", (d) => x(d))
-      .attr("y1", 0)
-      .attr("y2", height)
-      .attr("stroke", "#e0e0e0")
-      .attr("stroke-width", 0.5);
-
-    // y-axis grid-line
-    svg
-      .selectAll("yGrid")
-      .data(y.ticks(d3.max(data.price, (d) => d.y - yMin) / 4500).slice(1, -1))
-      .join("line")
-      .attr("x1", 0)
-      .attr("x2", width)
-      .attr("y1", (d) => y(d))
-      .attr("y2", (d) => y(d))
-      .attr("stroke", "#e0e0e0")
-      .attr("stroke-width", 0.5);
-
-    // Individual vertical line (connects y & x)
-    const line = d3
-      .line<{ x: number; y: number }>()
-      .x((d) => x(new Date(d.x)))
-      .y((d) => y(d.y));
-
-    const area = d3
-      .area<{ x: number; y: number }>()
-      .x((d) => x(new Date(d.x)))
-      .y0(height)
-      .y1((d) => y(d.y));
-
-    svg
-      .append("path")
-      .datum(data.price)
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-width", 2)
-      .attr("d", line);
-
-    svg
-      .append("path")
-      .datum(data.price)
-      .attr("class", "area") // Add this class so your slider can find it
-      .attr("fill", "steelblue")
-      .attr("opacity", 0.2)
-      .attr("d", area);
-
-    // hovering svg
-    const circle = svg
-      .append("circle")
-      .attr("r", 0)
-      .attr("fill", "steelblue")
-      .style("stroke", "white")
-      .attr("opacity", 0.7)
-      .style("pointer-events", "none");
-
-    const listeningRect = svg
-      .append("rect")
-      .attr("class", classes.rect)
-      .attr("width", width)
-      .attr("height", height);
-
-    // tooltip - mousemovement tracker
-    listeningRect.on("mousemove", function (event) {
-      const [xCoord] = d3.pointer(event);
-      const x0 = x.invert(xCoord);
-      const bisectDate = d3.bisector((d) => new Date(d.x)).left;
-
-      const i = bisectDate(data.price, x0, 1);
-      const d0 = data.price[i - 1];
-      const d1 = data.price[i];
-
-      const d =
-        x0.getTime() - new Date(d0.x).getTime() >
-        new Date(d1.x).getTime() - x0.getTime()
-          ? d1
-          : d0;
-
-      circle.attr("cx", x(new Date(d.x))).attr("cy", y(d.y));
-      circle.transition().duration(50).attr("r", 5);
-
-      tooltip
-        .style("display", "block")
-        .style("left", `${x(new Date(d.x)) + 100}px`)
-        .style("top", `${y(d.y) + 75}px`)
-        .html(
-          `<strong>Time:</strong> ${new Date(d.x).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}<br><strong>Price:</strong>${d.y}`,
-        );
-    });
-
-    listeningRect.on("mouseleave", function () {
-      circle.transition().duration(50).attr("r", 0);
-
-      tooltip.style("display", "none");
-    });
-
-    // define sliderRange and its time-range
-    const sliderRange = sliderBottom()
-      .min(d3.min(data.price, (d) => new Date(d.x)))
-      .max(d3.max(data.price, (d) => new Date(d.x)))
-      .width(300)
-      .tickFormat(d3.timeFormat("%H:%M"))
-      .ticks(3)
-      .default([
-        d3.min(data.price, (d) => new Date(d.x)),
-        d3.max(data.price, (d) => new Date(d.x)),
-      ])
-      .fill("#85bb65");
-
-    sliderRange.on("onchange", (val) => {
-      x.domain(val);
-
-      const filteredData = data.price.filter(
-        (d) => new Date(d.x) >= val[0] && new Date(d.x) <= val[1],
-      );
-
-      svg.select(".line").datum(filteredData).attr("d", line);
-
-      svg.select(".area").attr("d", area(filteredData)); // not sure what he means by area
-
-      y.domain([0, d3.max(filteredData, (d) => d.y) as number]).nice();
-
-      svg
-        .select(".x-axis")
-        .transition()
-        .duration(300)
-        .call(
-          d3
-            .axisBottom(x)
-            .tickValues(x.ticks(d3.timeYear.every(1)))
-            .tickFormat(d3.timeFormat("%Y")),
-        );
-
-      svg
-        .select(".y-axis")
-        .transition()
-        .duration(300)
-        .call(
-          d3
-            .axisRight(y)
-            .ticks(10)
-            .tickFormat((d) => {
-              if (d <= 0) return "";
-
-              return `${d.toFixed(2)}`;
-            }),
-        );
-    });
-
-    const gRange = d3
-      .select("#slider-range")
-      .append("svg")
-      .attr("width", 500)
-      .attr("height", 100)
-      .append("g")
-      .attr("transform", `translate(90,30)`);
-
-    gRange.call(sliderRange);
-
-    // graph title
-    svg
-      .append("text")
-      .attr("class", "chart-title")
-      .attr("x", margin.left - 115)
-      .attr("y", margin.top - 100)
-      .style("font-size", "18px")
-      .style("font-weight", "thin")
-      .style("font-family", "sans-serif")
-      .text("Bitcoin-BTCby times: ... : ...");
-  }, [data]);
-
-  if (isLoading) return <div>Loading...</div>;
-
-  return (
-    <>
-      <div className="chart-container" ref={chartContainer} />;
-      <div id="slider-range" />
-    </>
-  );
-};
-
 const GraphTwo = () => {
   const { data, isLoading } = useQuery({
     queryKey: ["graphData"],
@@ -299,20 +23,24 @@ const GraphTwo = () => {
 
   const chartContainer = useRef<HTMLDivElement | null>(null);
 
+  // chartContainer dimensions
   const margin = { top: 70, right: 60, bottom: 50, left: 80 };
   const width = 1400 - margin.left - margin.right;
   const height = 800 - margin.top - margin.bottom;
 
+  // chartContainer scales
   const x = d3.scaleTime().range([0, width]);
   const y = d3.scaleLinear().range([height, 0]);
 
   useEffect(() => {
     if (!chartContainer.current || isLoading) return;
 
+    // Clean chart once data updates
     d3.select(chartContainer.current).selectAll("svg").remove();
 
     const refEl = chartContainer?.current;
 
+    // Main-svg
     const svg = d3
       .select(refEl)
       .append("svg")
@@ -320,28 +48,76 @@ const GraphTwo = () => {
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
       .attr("transform", `translate(${margin.left},${margin.right})`);
+    const toolTip = d3
+      .select("body")
+      .append("div")
+      .attr("class", classes.toolTip);
+
+    const tooltipRawDate = d3
+      .select("body")
+      .append("div")
+      .attr("class", classes.toolTip);
+
+    // since price fluxes at a very high number, base minimum/maximum on percentace instead of 0/max
+    const yMin = (d3.min(data.price, (d: Price) => d.y) * 0.8) as number,
+      yMax = (d3.max(data.price, (d: Price) => d.y) * 1.1) as number;
 
     x.domain([data.from, data.to]) as [Date, Date];
-
-    const yMin = (d3.min(data.price, (d) => d.y) - 20000) as number;
-    const yMax = (d3.max(data.price, (d) => d.y) + 20000) as number;
-
     y.domain([yMin, yMax]);
 
-    svg
-      .append("g")
-      .attr("transform", `translate(0,${height})`)
-      .call(d3.axisBottom(x));
+    const gradient = svg
+      .append("defs")
+      .append("linearGradient")
+      .attr("id", "gradient")
+      .attr("x1", "0%")
+      .attr("x2", "0%")
+      .attr("y1", "0%")
+      .attr("y2", "100%")
+      .attr("spreadMethod", "pad");
+
+    gradient
+      .append("stop")
+      .attr("offset", "0%")
+      .attr("stop-color", "#85bb65")
+      .attr("stop-opacity", 1);
+
+    gradient
+      .append("stop")
+      .attr("offset", "100%")
+      .attr("stop-color", "#85bb65")
+      .attr("stop-opacity", 0);
 
     svg
       .append("g")
-      .attr("transform", `translate(${width},0)`)
+      .attr("class", "x-axis")
+      .attr("transform", `translate(0,${height})`)
+      .style("font-size", "14px")
       .call(
-        d3.axisRight(y).tickFormat((d) => {
-          if (isNaN(d)) return "";
-          return `${d.toFixed(2)}`;
-        }),
-      );
+        d3
+          .axisBottom(x)
+          .tickValues(x.ticks(d3.timeYear.every(1)))
+          .tickFormat(d3.timeFormat("%Y")),
+      )
+      .selectAll(".tick line")
+      .style("stroke-opacity", 1);
+    svg.selectAll(".tick text").attr("fill", "#777");
+
+    svg
+      .append("g")
+      .attr("class", "y-axis")
+      .attr("transform", `translate(${width},0)`)
+      .style("font-size", "14px")
+      .call(
+        d3
+          .axisRight(y)
+          .ticks(10)
+          .tickFormat((d: number) => {
+            if (isNaN(d)) return "";
+            return `${d.toFixed(2)}`;
+          }),
+      )
+      .selectAll(".tick text")
+      .style("fill", "+777");
 
     const line = d3
       .line()
@@ -359,7 +135,7 @@ const GraphTwo = () => {
       .datum(data.price)
       .attr("class", "area")
       .attr("d", area)
-      .style("fill", "#85bb65")
+      .style("fill", "url(#gradient")
       .style("opacity", 0.5);
 
     svg
@@ -370,6 +146,155 @@ const GraphTwo = () => {
       .attr("stroke", "#85bb65")
       .attr("stroke-width", 1)
       .attr("d", line);
+
+    const circle = svg
+      .append("circle")
+      .attr("r", 0)
+      .attr("fill", "red")
+      .style("stroke", "white")
+      .attr("opacity", 0.7)
+      .style("pointer-events", "none");
+
+    const tooltipLineX = svg
+      .append("line")
+      .attr("class", classes.tooltipLine)
+      .attr("id", "tooltip-line-x")
+      .attr("stroke", "red")
+      .attr("stroke-width", 1)
+      .attr("stroke-dasharray", "2,2");
+
+    const tooltipLineY = svg
+      .append("line")
+      .attr("class", classes.tooltipLine)
+      .attr("id", "tooltip-line-y")
+      .attr("stroke", "red")
+      .attr("stroke-width", 1)
+      .attr("stroke-dasharray", "2,2");
+
+    const listeningRect = svg
+      .append("rect")
+      .attr("class", classes.rect)
+      .attr("width", width)
+      .attr("height", height);
+
+    listeningRect.on("mousemove", (event: MouseEvent) => {
+      const [xCoord] = d3.pointer(event, this);
+      const bisectDate = d3.bisector((d: Price) => d.x).left;
+
+      const x0 = x.invert(xCoord);
+      const i = bisectDate(data.price, x0, 1);
+      const d0 = data.price[i - 1];
+      const d1 = data.price[i];
+      const d = x0 - d0.x > d1.x - x0 ? d1 : d0;
+
+      const xPos = x(d.x);
+      const yPos = y(d.y);
+
+      circle.attr("cx", xPos).attr("cy", yPos);
+
+      circle.transition().duration(50).attr("r", 5);
+
+      tooltipLineX
+        .style("display", "block")
+        .attr("x1", xPos)
+        .attr("x2", xPos)
+        .attr("y1", 0)
+        .attr("y2", height);
+
+      tooltipLineY
+        .style("display", "block")
+        .attr("y1", yPos)
+        .attr("y2", yPos)
+        .attr("x1", 0)
+        .attr("x2", width);
+
+      toolTip
+        .style("display", "block")
+        .style("left", `${width + 90}px`)
+        .style("top", `${yPos + 68}px`)
+        .html(`${d.y !== undefined ? d.y.toFixed(2) : "N/A"}`);
+
+      tooltipRawDate
+        .style("display", "block")
+        .style("left", `${xPos + 60}px`)
+        .style("top", `${height + 53}px`)
+        .html(`${d.x !== undefined ? d.x.toISOString().slice(0, 10) : "N/A"}`);
+    });
+
+    listeningRect.on("mouseleave", () => {
+      circle.transition().duration(50).attr("r", 0);
+      toolTip.style("display", "none");
+      tooltipRawDate.style("display", "none");
+      tooltipLineX.attr("x1", 0).attr("x2", 0);
+      tooltipLineY.attr("y1", 0).attr("y2", 0);
+      tooltipLineX.style("display", "none");
+      tooltipLineY.style("display", "none");
+    });
+
+    const sliderRange = sliderBottom()
+      .min(data.min)
+      .max(data.max)
+      .width(300)
+      .tickFormat(d3.timeFormat("%Y-%m-%d"))
+      .ticks(3)
+      .default([data.from, data.to])
+      .fill("#85bb65");
+
+    sliderRange.on("onchange", (val) => {
+      x.domain(val);
+
+      const filteredData = data.filter((d) => d.x >= val[0] && d.x <= val[1]);
+
+      svg.select(".line").attr("d", line(filteredData));
+      svg.select(".area").attr("d", area(filteredData));
+
+      y.domain([0, d3.max(filteredData, (d) => d.x)]);
+
+      svg
+        .select(".x-axis")
+        .transition()
+        .duration(300)
+        .call(
+          d3
+            .axisBottom(x)
+            .tickValues(x.ticks(d3.timeYear.every(1)))
+            .tickFormat(d3.timeFormat("%Y")),
+        );
+    });
+
+    svg
+      .select(".y-axis")
+      .transition()
+      .duration(300)
+      .call(
+        d3
+          .axisRight(y)
+          .ticks(10)
+          .tickFormat((d) => {
+            if (d <= 0) return "";
+            return `${d.toFixed(2)}`;
+          }),
+      );
+
+    const gRange = d3
+      .select("#slider-range")
+      .append("svg")
+      .attr("width", 500)
+      .attr("height", 100)
+      .append("g")
+      .attr("transform", "translate(90,30)");
+
+    gRange.call(sliderRange);
+
+    svg
+      .append("text")
+      .attr("class", "chart-title")
+      .attr("x", margin.left - 115)
+      .attr("y", margin.top - 100)
+      .style("font-size", "20px")
+      .style("font-weight", "bold")
+      .style("font-family", "sans-serif")
+      .text("Bitcoin Price Over Time");
   }, [
     data,
     isLoading,
---
+++ src/components/graphOne.tsx
@@ -0,0 +1,277 @@
+const GraphOne = () => {
+  const chartContainer = useRef<HTMLDivElement | null>(null);
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["graphData"],
+    queryFn: () => fetchData("/data.json"),
+  });
+
+  useEffect(() => {
+    if (isLoading) return;
+
+    // Clean chart
+    d3.select(chartContainer.current).selectAll("svg").remove();
+
+    const margin = { top: 70, right: 30, bottom: 40, left: 80 };
+    const width = 1200 - margin.left - margin.right;
+    const height = 500 - margin.top - margin.bottom;
+
+    // Main-svg
+    const svg = d3
+      .select(chartContainer.current)
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // tooltip - mouse over svg
+    const tooltip = d3
+      .select("body")
+      .append("div")
+      .append("tooltip")
+      .attr("class", `${classes.tooltip}`);
+
+    // Scales
+    // data.price.x === date
+    const x = d3
+      .scaleTime()
+      .range([0, width])
+      .domain(d3.extent(data.price, (d) => new Date(d.x)) as [Date, Date]);
+
+    // data.price.y === price
+    const yMin = d3.min(data.price, (d) => d.y) as number;
+    const yMax = d3.max(data.price, (d) => d.y) as number;
+    const y = d3
+      .scaleLinear()
+      .domain([yMin - 10000, yMax + 10000])
+      .range([height, 0])
+      .nice();
+
+    svg
+      .append("g")
+      .attr("transform", `translate(0,${height})`)
+      .style("font-size", "12px")
+      .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%H:%M")))
+      .call((g) => g.select(".domain").remove().selectAll(".tick text"))
+      .style("stroke-opacity", 0);
+    svg.selectAll(".tick text").attr("fill", "#777");
+
+    // y-axis
+    svg
+      .append("g")
+      .style("font-size", "12px")
+      .call(
+        d3
+          .axisLeft(y)
+          .ticks(d3.max(data.price, (d) => d.y - yMin) / 4500) // Increase/decrease to adjust visible price-points
+          .tickFormat((d) => {
+            const num: number = d;
+
+            if (num >= 10000) {
+              return `${(num / 1000).toFixed(0)}k`;
+            } else {
+              num.toString();
+            }
+          })
+          .tickSize(0)
+          .tickPadding(10),
+      )
+      .call((g) => g.select(".domain").remove())
+      .selectAll(".tick text")
+      .style("fill", "#777")
+      .style("visibility", (_, i, __) => {
+        if (i === 0) {
+          return "hidden";
+        } else {
+          return "visible";
+        }
+      });
+
+    // x-axis grid-line
+    svg
+      .selectAll("xGrid")
+      .data(x.ticks())
+      .join("line")
+      .attr("x1", (d) => x(d))
+      .attr("x2", (d) => x(d))
+      .attr("y1", 0)
+      .attr("y2", height)
+      .attr("stroke", "#e0e0e0")
+      .attr("stroke-width", 0.5);
+
+    // y-axis grid-line
+    svg
+      .selectAll("yGrid")
+      .data(y.ticks(d3.max(data.price, (d) => d.y - yMin) / 4500).slice(1, -1))
+      .join("line")
+      .attr("x1", 0)
+      .attr("x2", width)
+      .attr("y1", (d) => y(d))
+      .attr("y2", (d) => y(d))
+      .attr("stroke", "#e0e0e0")
+      .attr("stroke-width", 0.5);
+
+    // Individual vertical line (connects y & x)
+    const line = d3
+      .line<{ x: number; y: number }>()
+      .x((d) => x(new Date(d.x)))
+      .y((d) => y(d.y));
+
+    const area = d3
+      .area<{ x: number; y: number }>()
+      .x((d) => x(new Date(d.x)))
+      .y0(height)
+      .y1((d) => y(d.y));
+
+    svg
+      .append("path")
+      .datum(data.price)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 2)
+      .attr("d", line);
+
+    svg
+      .append("path")
+      .datum(data.price)
+      .attr("class", "area") // Add this class so your slider can find it
+      .attr("fill", "steelblue")
+      .attr("opacity", 0.2)
+      .attr("d", area);
+
+    // hovering svg
+    const circle = svg
+      .append("circle")
+      .attr("r", 0)
+      .attr("fill", "steelblue")
+      .style("stroke", "white")
+      .attr("opacity", 0.7)
+      .style("pointer-events", "none");
+
+    const listeningRect = svg
+      .append("rect")
+      .attr("class", `rect ${classes.rect}`)
+      .attr("width", width)
+      .attr("height", height);
+
+    // tooltip - mousemovement tracker
+    listeningRect.on("mousemove", function (event) {
+      const [xCoord] = d3.pointer(event);
+      const x0 = x.invert(xCoord);
+      const bisectDate = d3.bisector((d) => new Date(d.x)).left;
+
+      const i = bisectDate(data.price, x0, 1);
+      const d0 = data.price[i - 1];
+      const d1 = data.price[i];
+
+      const d =
+        x0.getTime() - new Date(d0.x).getTime() >
+        new Date(d1.x).getTime() - x0.getTime()
+          ? d1
+          : d0;
+
+      circle.attr("cx", x(new Date(d.x))).attr("cy", y(d.y));
+      circle.transition().duration(50).attr("r", 5);
+
+      tooltip
+        .style("display", "block")
+        .style("left", `${x(new Date(d.x)) + 100}px`)
+        .style("top", `${y(d.y) + 75}px`)
+        .html(
+          `<strong>Time:</strong> ${new Date(d.x).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}<br><strong>Price:</strong>${d.y}`,
+        );
+    });
+
+    listeningRect.on("mouseleave", function () {
+      circle.transition().duration(50).attr("r", 0);
+
+      tooltip.style("display", "none");
+    });
+
+    // define sliderRange and its time-range
+    const sliderRange = sliderBottom()
+      .min(d3.min(data.price, (d) => new Date(d.x)))
+      .max(d3.max(data.price, (d) => new Date(d.x)))
+      .width(300)
+      .tickFormat(d3.timeFormat("%H:%M"))
+      .ticks(3)
+      .default([
+        d3.min(data.price, (d) => new Date(d.x)),
+        d3.max(data.price, (d) => new Date(d.x)),
+      ])
+      .fill("#85bb65");
+
+    sliderRange.on("onchange", (val) => {
+      x.domain(val);
+
+      const filteredData = data.price.filter(
+        (d) => new Date(d.x) >= val[0] && new Date(d.x) <= val[1],
+      );
+
+      svg.select(".line").datum(filteredData).attr("d", line);
+
+      svg.select(".area").attr("d", area(filteredData)); // not sure what he means by area
+
+      y.domain([0, d3.max(filteredData, (d) => d.y) as number]).nice();
+
+      svg
+        .select(".x-axis")
+        .transition()
+        .duration(300)
+        .call(
+          d3
+            .axisBottom(x)
+            .tickValues(x.ticks(d3.timeYear.every(1)))
+            .tickFormat(d3.timeFormat("%Y")),
+        );
+
+      svg
+        .select(".y-axis")
+        .transition()
+        .duration(300)
+        .call(
+          d3
+            .axisRight(y)
+            .ticks(10)
+            .tickFormat((d) => {
+              if (d <= 0) return "";
+
+              return `${d.toFixed(2)}`;
+            }),
+        );
+    });
+
+    const gRange = d3
+      .select("#slider-range")
+      .append("svg")
+      .attr("width", 500)
+      .attr("height", 100)
+      .append("g")
+      .attr("transform", `translate(90,30)`);
+
+    gRange.call(sliderRange);
+
+    // graph title
+    svg
+      .append("text")
+      .attr("class", "chart-title")
+      .attr("x", margin.left - 115)
+      .attr("y", margin.top - 100)
+      .style("font-size", "18px")
+      .style("font-weight", "thin")
+      .style("font-family", "sans-serif")
+      .text("Bitcoin-BTCby times: ... : ...");
+  }, [data]);
+
+  if (isLoading) return <div>Loading...</div>;
+
+  return (
+    <>
+      <div className="chart-container" ref={chartContainer} />;
+      <div id="slider-range" />
+    </>
+  );
+};
+
+export default GraphOne;
